663f40c9d06e81e1c6938d08be063c5f8ce02481
Jakub Wudarski <jakub.wudarski@idealgroup.com.pl>
2023-01-12 16:49:48 +0100

    routers add to app.js

--- firstWebsite/app.js
+++ firstWebsite/app.js
@@ -5,7 +5,9 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var artistRouter = require('./routes/artistRouter');
+var concertRouter = require('./routes/concertRouter');
+var artistOnConcertRouter = require('./routes/artistOnConcertRouter');
 
 var app = express();
 
@@ -20,7 +22,10 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/artist', artistRouter);
+app.use('/concert', concertRouter);
+app.use('/artistOnConcert', artistOnConcertRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
