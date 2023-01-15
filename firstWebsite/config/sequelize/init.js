0f6ace280e50bfd8eb92f26ecd36638390fd0c79
Kuba Wudarski <kuba@wudarscy.pl>
2023-01-15 19:11:17 +0100

    db init fixes

--- firstWebsite/config/sequelize/init.js
+++ firstWebsite/config/sequelize/init.js
@@ -1,19 +1,52 @@
-const sequelize = require('sequelize');
+const sequelize = require('./sequelize');
 
 const Artist = require('../../model/sequelize/Artist');
 const Concert = require('../../model/sequelize/Concert');
 const ArtistOnConcert = require('../../model/sequelize/ArtistOnConcert');
 
 module.exports = () => {
-    ArtistOnConcert.hasMany(Artist, {as: 'artists', foreingKey: {name: 'ArtistId', allowNull: false}, constraint: true, onDelete: 'CASCADE'});
-    Artist.belongsTo(ArtistOnConcert, {as: 'artistOnConcert', foreignKey: {name: 'ArtistId', allowNull: false}, constraint: true});
-    ArtistOnConcert.hasMany(Concert, {as: 'concerts', foreignKey: {name: 'ConcertId', allowNull: false}, constraint: true, onDelete: 'CASCADE'});
-    Concert.belongsTo(ArtistOnConcert, {as: 'artistOnConcert', foreignKey: {name: 'ConcertId', allowNull: false}, constraint: true});
+    Artist.hasMany(
+        ArtistOnConcert,
+        {
+            as: 'artist',
+            foreignKey: { name: 'ArtistId', allowNull: false },
+            constraint: true,
+            onDelete: 'CASCADE',
+            onUpdate: 'CASCADE',
+        }
+    );
+
+    Concert.hasMany(
+        ArtistOnConcert,
+        {
+            as: 'concert',
+            foreignKey: { name: 'ConcertId', allowNull: false},
+            constraint: true,
+            onDelete: 'CASCADE',
+            onUpdate: 'CASCADE',
+        }
+    );
+
+    ArtistOnConcert.belongsTo(
+        Artist,
+        {
+            as: 'artistOnConcert',
+            foreignKey: { name: 'ArtistId' }
+        }
+    );
+
+    ArtistOnConcert.belongsTo(
+        Concert,
+        {
+            as: 'ArtistOnConcert',
+            foreignKey: { name: 'ConcertId' },
+        }
+    );
 
     let allArtists, allConcerts;
 
     return sequelize
-        .sync({force: true})
+        .sync({ force: true })
         .then(() => {
             return Artist.findAll();
         })
@@ -22,9 +55,9 @@ module.exports = () => {
                 return Artist.bulkCreate([
                     {FirstName: "Kuba", LastName: "Wudrski", Pseudonym: "Student"},
                     {FirstName: "Marek", LastName: "T", Pseudonym: "T.M", Birthdate: "2000-03-13"},
-                    {FirstName: "Arek", LastName: "U", Pseudonym: "", Birthdate: "2012-09-20"},
+                    {FirstName: "Arek", LastName: "U", Birthdate: "2012-09-20"},
                     {FirstName: "Darek", LastName: "D", Pseudonym: "DD", Birthdate: "1890-12-29"},
-                    {FirstName: "Starek", LastName: "Arek", Pseudonym: "", Birthdate: "1999-11-25"},
+                    {FirstName: "Starek", LastName: "Arek", Birthdate: "1999-11-25"},
                 ])
                     .then(() => {
                         return Artist.findAll();
@@ -63,10 +96,10 @@ module.exports = () => {
                     {ArtistId: "2", ConcertId: "3", PerformanceNumber: "1", PerformanceTime: "60"},
                     {ArtistId: "3", ConcertId: "1", PerformanceNumber: "0", PerformanceTime: "75"},
                     {ArtistId: "4", ConcertId: "1", PerformanceNumber: "1", PerformanceTime: "30"},
-                    {ArtistId: "0", ConcertId: "1", PerformanceNumber: "2", PerformanceTime: "35"},
-                ]);
+                    {ArtistId: "5", ConcertId: "1", PerformanceNumber: "2", PerformanceTime: "35"},
+                ])
             } else {
                 return artistOnConcert;
             }
-        })
-}
+        });
+};
