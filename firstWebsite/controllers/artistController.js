77dadac25e8991d263497c2433d2693dbc75f9ef
Kuba Wudarski <kuba@wudarscy.pl>
2023-01-15 14:17:44 +0100

    controlers update

--- firstWebsite/controllers/artistController.js
+++ firstWebsite/controllers/artistController.js
@@ -1,11 +1,11 @@
 exports.showArtistList = (req, res, next) => {
     res.render('Pages/Artist/list', { navLocation: 'artist' })
-}
+};
 
 exports.showAddArtistForm = (req, res, next) => {
     res.render('Pages/Artist/form', { navLocation: 'artist' });
-}
+};
 
 exports.showArtistDetails = (req, res, next) => {
-    res.render('Pages/Artist/', { navLocation: 'artist' });
-}
+    res.render('Pages/Artist/form-details', { navLocation: 'artist' });
+};
--- firstWebsite/controllers/artistOnConcertController.js
+++ firstWebsite/controllers/artistOnConcertController.js
@@ -1,11 +1,11 @@
 exports.showArtistOnConcertList = (req, res, next) => {
     res.render('Pages/ArtistOnConcert/list', { navLocation: 'artistOnConcert' });
-}
+};
 
 exports.showAddArtistOnConcertForm = (req, res, next) => {
     res.render('Pages/ArtistOnConcert/from', { navLocation: 'artistOnConcert' });
-}
+};
 
 exports.showArtistOnConcertDetails = (req, res, next) => {
-    res.render('Pages/ArtistOnConcert/details', { navLocation: 'artistOnConcert' });
-}
+    res.render('Pages/ArtistOnConcert/form-details', { navLocation: 'artistOnConcert' });
+};
