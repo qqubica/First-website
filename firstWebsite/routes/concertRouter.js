444928b0465ce0a39fcffc30856347f93f2ffa59
Kuba Wudarski <kuba@wudarscy.pl>
2023-01-18 20:44:58 +0100

    missing /

--- firstWebsite/routes/concertRouter.js
+++ firstWebsite/routes/concertRouter.js
@@ -5,6 +5,11 @@ const concertController = require('../controllers/concertController');
 
 router.get('/', concertController.showConcertList);
 router.get('/add', concertController.showAddConcertForm);
-router.get('details/:concertId', concertController.showConcertDetails);
+router.get('/edit/:concertId', concertController.showEditConcertDetails);
+router.get('/details/:concertId', concertController.showConcertDetails);
+
+router.post('/add', concertController.addConcert);
+router.post('/edit', concertController.updateConcert);
+router.post('/delete/:id', concertController.deleteConcert);
 
 module.exports = router;
