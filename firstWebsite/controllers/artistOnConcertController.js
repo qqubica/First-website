9ce4327e9df54a73e1de61a470b5795381d20253
Jakub Wudarski <jakub.wudarski@idealgroup.com.pl>
2023-01-19 10:45:31 +0100

    dynamic table

--- firstWebsite/controllers/artistOnConcertController.js
+++ firstWebsite/controllers/artistOnConcertController.js
@@ -1,5 +1,25 @@
+const ConcertRepository = require('../repository/sequelize/ConcertRepository');
+const ArtistOnConcerRepository = require('../repository/sequelize/ArtistOnConcertRepository')
+const ArtistRepository = require('../repository/sequelize/ArtistRepository');
+
 exports.showArtistOnConcertList = (req, res, next) => {
-    res.render('Pages/ArtistOnConcert/list', { navLocation: 'artistOnConcert' });
+    ArtistOnConcerRepository.getArtistsOnConcerts()
+        .then(aOc => {
+            return aOc.map(x => {
+                return {
+                    firstName: x.dataValues.artistOnConcert.dataValues.FirstName,
+                    lastName: x.dataValues.artistOnConcert.dataValues.LastName,
+                    pseudonym: x.dataValues.artistOnConcert.dataValues.Pseudonym,
+                    venue: x.dataValues.performedDuring.dataValues.Venue,
+                }
+            })
+        })
+        .then(listData => {
+            res.render('Pages/ArtistOnConcert/list', {
+                artistsOnConcerts: listData,
+                navLocation: 'artistOnConcert'
+            });
+        })
 };
 
 exports.showAddArtistOnConcertForm = (req, res, next) => {
